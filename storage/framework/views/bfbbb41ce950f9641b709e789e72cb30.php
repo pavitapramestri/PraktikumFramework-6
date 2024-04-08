f2a0e244c5d77e54520daff4f75e2038fc1d82f1
pavitapramestri <151427927+pavitapramestri@users.noreply.github.com>
2024-04-09 02:04:38 +0700

    Add files via upload

---
+++ storage/framework/views/bfbbb41ce950f9641b709e789e72cb30.php
@@ -0,0 +1,14 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="container mt-4">
+        <h4><?php echo e($pageTitle); ?></h4>
+        <hr>
+        <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
+            <div class="bi-house-fill me-3 fs-1"></div>
+            <h4 class="mb-0">Well done! this is <?php echo e($pageTitle); ?>.</h4>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Laravel\modul4\resources\views/home.blade.php ENDPATH**/ ?>
